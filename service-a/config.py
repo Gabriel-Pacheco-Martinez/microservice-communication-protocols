12d4e14e361dea990668d570df91f28881251df5
Gabriel-Pacheco-Martinez <gabrielpachecomartinez2000@gmail.com>
2025-11-03 09:27:11 -0500

    Added postgresql

--- service-a/config.py
+++ service-a/config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # load .env file
 
 DB_HOST = os.getenv("DB_HOST", "localhost")  # Docker maps port to host port
 DB_PORT = os.getenv("DB_PORT", "5432")
